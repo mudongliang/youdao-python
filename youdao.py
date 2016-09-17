40aded97a65399f767d7c250654afd630d09d9fc
Dongliang Mu <mudongliangabcd@gmail.com>
2016-09-16 23:02:36 -0400

    PEP8 Check

--- youdao.py
+++ youdao.py
@@ -22,7 +22,9 @@ def fetch(query_str=''):
     query = {
         'q': query_str
     }
-    url = 'http://fanyi.youdao.com/openapi.do?keyfrom=youdao-python&key=701380394&type=data&doctype=json&version=1.1&' + urlencode(query)
+    url = 'http://fanyi.youdao.com/openapi.do?' + \
+          'keyfrom=youdao-python&key=701380394' + \
+          '&type=data&doctype=json&version=1.1&' + urlencode(query)
     response = urlopen(url, timeout=3)
     html = response.read().decode('utf-8')
     return html
@@ -31,19 +33,19 @@ def fetch(query_str=''):
 def parse(html):
     d = json.loads(html)
     try:
-        if d.get('errorCode') == 0 :
-            explains = d.get('basic').get('explains')
-            for explain in explains:
+        if d.get('errorCode') == 0:
+            basicexplains = d.get('basic').get('explains')
+            for explain in basicexplains:
                 print(explain)
-        elif d.get('errorCode' == 20) :
+        elif d.get('errorCode' == 20):
             print('要翻译的文本过长')
-        elif d.get('errorCode' == 30) :
+        elif d.get('errorCode' == 30):
             print('无法进行有效的翻译')
-        elif d.get('errorCode' == 40) :
+        elif d.get('errorCode' == 40):
             print('不支持的语言类型')
-        elif d.get('errorCode' == 50) :
+        elif d.get('errorCode' == 50):
             print('无效的key')
-        else :
+        else:
             print('无词典结果，仅在获取词典结果生效')
     except:
         print('翻译出错，请输入合法单词')
